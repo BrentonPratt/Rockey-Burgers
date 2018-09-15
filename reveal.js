22840a70e252913a85a6fc5dc8e74874453362b9
BrentonPratt <42676802+BrentonPratt@users.noreply.github.com>
2018-09-15 12:17:19 -0600

    Add JS file for hide/show function

---
+++ reveal.js
@@ -0,0 +1,17 @@
+function myFunction() {
+    var x = document.getElementById("expensive");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
+
+function myFunction2() {
+    var y = document.getElementById("beef");
+    if (y.style.display === "block") {
+        y.style.display = "none";
+    } else {
+        y.style.display = "block";
+    }
+}
